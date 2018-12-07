b452a78823e66138727e0fa1299d9fa4af11b8ba
tgpereira <thiago.pereira@connectcom.univesp.br>
2018-12-07 13:49:22 -0200

    Creates table and adds components

---
+++ src/components/Content/index.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Jumbotron from '../Jumbotron';
+import Audit from '../../pages/Audit';
+
+import Main from './styles';
+import Avaliations from '../../assets/imgs/avaliacoes.svg';
+
+const Content = () => (
+  <Fragment>
+    <Jumbotron title="APOIO AO ALUNO" icon={Avaliations} />
+    <Main>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Audit}></Route>
+        </Switch>
+      </BrowserRouter>
+    </Main>
+  </Fragment>
+);
+
+export default Content;
