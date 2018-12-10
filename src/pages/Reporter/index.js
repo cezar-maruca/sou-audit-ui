266e5e99eb42882034e92555acfc7253dfb9bd2d
cezar.maruca <cezar.maruca@connectcom.univesp.br>
2018-12-10 11:43:05 -0200

    Feito a página reporter.js

---
+++ src/pages/Reporter/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const Report = () => {
+  return (
+    <div>
+      <div className="container">
+        <hr />
+        <h5>Dados Pessoais</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Report;
